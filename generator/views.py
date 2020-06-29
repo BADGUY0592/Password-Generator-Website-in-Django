3d70f9df1df2d64d2fa1ae3ad2267bea445bd1c6
BADGUY0592 <53206810+BADGUY0592@users.noreply.github.com>
2020-06-29 20:18:20 +0530

    add random password generating functionality

--- generator/views.py
+++ generator/views.py
@@ -1,9 +1,22 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def home(request):
     return render(request, 'generator/home.html')
 
 def generate(request):
-    return render(request, 'generator/rand.html')
+    if request.method == "GET":
+        lchar=list('qwertyuiopasdfghjklzxcvbnm')
+        length=int(request.GET.get('length'))
+        if(request.GET.get('case1')):
+            lchar.extend(list('QWERTYUIOPASDFGHJKLZXCVBNM'))
+        if(request.GET.get('case2')):
+            lchar.extend(list('1234567890'))
+        if(request.GET.get('case3')):
+            lchar.extend(list('~!@#$%^&*()_+=-[]:;"<>,.?/'))
+        rand = ''
+        for x in range(length):
+            rand+=random.choice(lchar)
+        return render(request, 'generator/rand.html',{'rand':rand})
